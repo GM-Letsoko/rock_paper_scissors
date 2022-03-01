3b2d7a2a605c5ecd83f39356954e830944406d98
George <letsokomolefe02@gmail.com>
2022-03-01 15:46:01 +0200

    added playRound function to script, at this point the winner is determined after 1 round

--- script.js
+++ script.js
@@ -5,4 +5,17 @@ const computerPlay = () => {
 	else return 'scissors';
 }
 
-console.log(computerPlay());
+const playRound = (playerSelection, computerSelection) => { 
+	if(playerSelection === 'rock' && computerSelection === 'paper') return 'You Lose! Paper beats Rock';
+	else if(playerSelection === 'paper' && computerSelection === 'rock') return 'You Win! Paper beats Rock';
+	else if(playerSelection === 'scissors' && computerSelection === 'paper') return 'You Win! Scissors beats Paper';
+	else if(playerSelection === 'paper' && computerSelection === 'scissors') return 'You Lose! Scissors beats Paper';
+	else if(playerSelection === 'rock' && computerSelection === 'scissors') return 'You Win! Rock beats Scissors';
+	else if(playerSelection === 'scissors' && computerSelection === 'rock') return 'You Lose! Rock beats Scissors';
+	else return 'Draw. Nobody wins.';
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerPlay()));
